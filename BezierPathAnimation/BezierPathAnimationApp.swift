135e431a1665b9896c669987b7d5d83a2078c48a
Blezin <blezin2205@gmail.com>
2021-04-12 20:52:31 +0300

    Initial Commit

---
+++ BezierPathAnimation/BezierPathAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierPathAnimationApp.swift
+//  BezierPathAnimation
+//
+//  Created by Blezin on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct BezierPathAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
